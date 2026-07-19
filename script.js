05ff75a6cb0088b6880a41e2006388978c35028f
ariyansijum77-lang <ariyansijum77@gmail.com>
2026-07-19 12:04:51 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function calculate() {
+    let expression = document.getElementById('display').value;
+    document.getElementById('display').value = eval(expression);
+}
